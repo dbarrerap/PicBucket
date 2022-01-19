9b557fe077b75867c1a8ba9e43ca1be2c888083b
David E. Barrera <dbarrerap@live.com>
2022-01-19 10:47:36 -0500

    Initial commit

---
+++ src/main/java/io/github/dbarrerap/picbucket/PicBucketApplication.java
@@ -0,0 +1,13 @@
+package io.github.dbarrerap.picbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicBucketApplication.class, args);
+    }
+
+}
