f82fa92214eaefe08b7a517212f7fbb3fb324647
David E. Barrera <dbarrerap@live.com>
2022-01-20 21:46:16 -0500

    Create UserController and test data

---
+++ src/main/java/io/github/dbarrerap/picbucket/controllers/UserController.java
@@ -0,0 +1,49 @@
+package io.github.dbarrerap.picbucket.controllers;
+
+import io.github.dbarrerap.picbucket.entities.Role;
+import io.github.dbarrerap.picbucket.entities.User;
+import io.github.dbarrerap.picbucket.services.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/users").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/roles").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/roles/assign")
+    public ResponseEntity<?> assignRole(@RequestBody RoleToUserForm form) {
+        userService.assignRole(form.getEmail(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+
+@Data
+class RoleToUserForm {
+    private String email;
+    private String roleName;
+}
